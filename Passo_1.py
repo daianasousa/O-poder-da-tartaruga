b364fa0c39fd4250c0b1b4a17aee5db4cd706647
daianasousa <daianasousa04121996@gmail.com>
2020-09-12 18:07:14 -0300

    criei o arquivo Passo_1.py

---
+++ Passo_1.py
@@ -0,0 +1,21 @@
+from turtle import *
+shape("turtle")
+speed(8)
+
+
+color('red')
+pensize(7)
+right(90)
+forward(100)
+left(90)
+forward(50)
+
+
+color('black')
+pensize(3)
+penup()
+forward(50)
+pendown()
+forward(50)
+
+done()
